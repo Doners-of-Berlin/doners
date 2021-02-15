d0407d8e2b1e6a77097cf45d925716a32026c3d8
ivan-user <username.web@protonmail.com>
2021-02-15 18:41:21 +0100

    Fixed the files

--- doners-of-berlin/routes/index.js
+++ doners-of-berlin/routes/index.js
@@ -5,6 +5,7 @@
 const express = require('express');
 const router  = express.Router();
 const Shops = require('../models/shop')
+
 /* GET home page */
 router.get('/', (req, res, next) => {
 Shops.find().then(shopsList => {
