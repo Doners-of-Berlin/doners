2f8ea25ee2c8f60735fd307bfe207e3a8e04f842
Khizr Saharan <khizrs@gmail.com>
2021-02-16 16:38:46 +0100

    added reviews form working

--- doners-of-berlin/routes/index.js
+++ doners-of-berlin/routes/index.js
@@ -1,53 +1,43 @@
+// const express = require('express').Router();
+// const router  = express.Router();
+
+
 const express = require('express');
 const router  = express.Router();
 const Shops = require('../models/shop')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index')
+  Shops.find().then(shopsList => {
+    console.log('-------',shopsList);
+    res.render('index', {shopsList});
+  });
 })
-
-router.get('/', (req, res, next) => {
-
-  Shops.coordinates.forEach(coord => {
-    console.log(coord)
-  })
-  .then(() => {
-    new mapboxgl.Marker()
-      .setLngLat(coord)
-      .addTo(map);
-  })
-  .catch(err => next(err))
-})
-
 
 
 router.get('/restaurant/:id', (req, res, next) => {
-  Shops.findById(req.params.id).then(shopsList => {
-    console.log('-------',shopsList);
-
-    res.render('restaurant', {shopsList});
+  Shops.findById(req.params.id)
+    .then(shopsList => {
+      console.log('-------',shopsList);
+      res.render('restaurant', {shopsList});
   });
-})
+  
+  })
   
   router.post("/restaurant/:id/restaurant-review", (req,res)=> {
     const {reviewername,review} = req.body
     const shopId = req.params.id
- 
 
-    Shops.findById(shopId).then(shop => {
+    // Shops.findById(shopId).then(shop => {
+      // shop.reviews.push({reviewername,review})
+  console.log("req.body",reviewername,review)
+  console.log("req.parans",shopId);
 
-      shop.reviews.push({reviewername,review})
-      Shops.findByIdAndUpdate(shopId, shop)
- 
-      console.log(shop );
-  
-  
-
-      
-      res.redirect("/restaurant/" + shopId)
-    }).catch(error => res.redirect("/celebrities/new" , {err}))
-    
+      Shops.findByIdAndUpdate(shopId, {
+        $push: {reviews: {reviewername, review}}
+      })
+        .then(() => res.redirect("/restaurant/" + shopId))
+        .catch((err) => console.log(err))
   })
 
 
