6943e7bbfb2f418f603519a08c01dfb46a30adde
ivan-user <username.web@protonmail.com>
2021-02-17 15:09:42 +0100

    The map is working with popups

--- doners-of-berlin/public/javascripts/mapbox.js
+++ doners-of-berlin/public/javascripts/mapbox.js
@@ -1,5 +1,3 @@
-const Shops = require('../models/shop')
-
 mapboxgl.accessToken = "pk.eyJ1IjoiYW5pYW5pYW5pYSIsImEiOiJja2w2ZjluM2YyN3BxMnZsYjkxenkwa2l3In0.ykZjMZE_daToUo2DDC0h8g";
 
 const map = new mapboxgl.Map({
@@ -10,9 +8,9 @@ const map = new mapboxgl.Map({
 });
 
 map.addControl(new mapboxgl.NavigationControl());
-
   
 function addDonnerPlace(lng, lat, name, address){
+  // new mapboxgl.Marker()
   new mapboxgl.Popup({closeButton: true})
     .setHTML("<strong>" + name + "</strong><p>" + address + "</p>")
     .setLngLat([lng,lat])
--- doners-of-berlin/routes/index.js
+++ doners-of-berlin/routes/index.js
@@ -1,7 +1,3 @@
-// const express = require('express').Router();
-// const router  = express.Router();
-
-
 const express = require('express');
 const router  = express.Router();
 const Shops = require('../models/shop')
@@ -10,27 +6,19 @@ const fileUploader = require('../configs/cloudinary.config');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  Shops.find()
-    .then((shops) => {
-      // shops.forEach(shop => {
-      //   console.log('coord:', shop.coordinates)
-        // new mapboxgl.Marker()
-        //   .setLngLat(coordinate)
-        //   .addTo(map);
-      res.render('index')
-    })
-  .catch(err => next(err))
-})
-
+  Shops.find().then(shopsList => {
+    // console.log('-------',shopsList);
+    res.render('index', {shopsList});
+  });
+  })
 
 router.get('/restaurant/:id', (req, res, next) => {
   Shops.findById(req.params.id)
     .then(shopsList => {
-      console.log('-------',shopsList);
+      // console.log('-------',shopsList);
       res.render('restaurant', {shopsList});
   });
-  
-  })
+})
   
   router.post("/restaurant/:id/restaurant-review", (req,res)=> {
     const {reviewername,review} = req.body
