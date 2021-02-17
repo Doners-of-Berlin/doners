f47321550748c8e4af57170289d4360a4cc2930a
ania stryjewska <ania.stryjewska@gmail.com>
2021-02-17 11:46:20 +0100

    added logout

--- doners-of-berlin/routes/auth.routes.js
+++ doners-of-berlin/routes/auth.routes.js
@@ -5,9 +5,8 @@ const User = require('../models/User.model');
 const bcrypt = require('bcrypt');
 const bcryptSalt = 10;
  
-
-router.get('/login', (req, res, next) => res.render('auth/login'));
 router.get('/signup', (req, res, next) => res.render('auth/signup'));
+router.get('/login', (req, res, next) => res.render('auth/login'));
  
 router.post('/signup', (req, res, next) => {
   const { username, password } = req.body;
@@ -72,9 +71,9 @@ router.post('/login', (req, res, next) => {
 
 
 router.get('/logout', (req, res) => {
- 
   req.logout();
   res.redirect('/');
 })
  
+
 module.exports = router;
