fe535b01baff57583f467360be4c0fab78b2f615
ivan-user <username.web@protonmail.com>
2021-02-17 17:36:17 +0100

    css updates and map changes

--- doners-of-berlin/public/javascripts/mapbox.js
+++ doners-of-berlin/public/javascripts/mapbox.js
@@ -4,15 +4,32 @@ const map = new mapboxgl.Map({
   container: "map", // container id
   style: "mapbox://styles/mapbox/streets-v11", // style URL
   center: [13.41053, 52.52437], // starting position [lng, lat]
-  zoom: 12 // starting zoom
+  zoom: 11 // starting zoom
 });
 
 map.addControl(new mapboxgl.NavigationControl());
   
-function addDonnerPlace(lng, lat, name, address){
-  // new mapboxgl.Marker()
-  new mapboxgl.Popup({closeButton: true})
-    .setHTML("<strong>" + name + "</strong><p>" + address + "</p>")
+//markers (pins) for doner shops
+function addDonnerMarker(lng, lat){
+  new mapboxgl
+    .Marker()
     .setLngLat([lng,lat])
     .addTo(map)
 }
+
+function addDonnerPopup(lng, lat, name, address){
+  new mapboxgl
+    .Popup({
+      anchor: 'bottom',
+      offset: 38})
+    .setHTML("<strong>" + name + "</strong><p>" + address + "</p>")
+    .setLngLat([lng,lat])
+    
+    .addTo(map)
+}
+
+// map.on('click', addDonnerPlace(), (lng, lat) => {
+//   new mapboxgl.Popup()
+//   .setLngLat(lng, lat)
+//   .addTo(map);
+//   });
